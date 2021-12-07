8edae3dd4c76faf2a351a36397238af40d5ad6c1
alsagun <61996937+alsagun@users.noreply.github.com>
2021-12-07 17:17:02 -0500

    Update assignment3Main.cpp

--- assignment3Main.cpp
+++ assignment3Main.cpp
@@ -1810,6 +1810,10 @@ void keyboard(unsigned char key, int x, int y)
 		exit(0);
 		break;
 	case 32:
+		laserV.laserXPos = towerX; 
+		laserV.laserZPos = zPos;
+		laserV.laserAngle = towerAngle;
+		laserV.fired = 1;
 		if (towerDMG == 3)
 		{
 			laserV.fired = 0;
